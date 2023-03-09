582b11c9529b3ce50a7d3c334a98630542460f90
Justin Huth <Jayhuth96@gmail.com>
2023-03-09 11:07:09 -0600

    Completed the excercises.

--- app.js
+++ app.js
@@ -3,8 +3,38 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 
+const firstname = "Jay";
+let lastname = "Huth";
+var age = 27;
 
 // Exercise 2
+lastname = "Berg";
+age = 72;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first released in ${createdYear}.');
+
+let product = "T-Shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = o;
+let inStock = false;
+selectedSize = "M";
+
+console.log('${product} in size ${selectedSize} is ${price} with ${stock} in stock.');
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasread = true;
+
+console.log('${title} by ${author} is ${pageCount} pages long. your last bookmark was at page ${bookmark}.');
+
